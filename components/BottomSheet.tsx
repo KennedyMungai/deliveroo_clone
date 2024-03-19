948a8e6c556fa04496ac27e49bef37d49fe41293
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 12:34:12 +0300

    Rendered out a BottomSheetBackDrop component as the backDropComponent

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,15 +1,27 @@
-import { BottomSheetModal } from '@gorhom/bottom-sheet'
-import React, { forwardRef, useMemo } from 'react'
+import { BottomSheetBackdrop, BottomSheetModal } from '@gorhom/bottom-sheet'
+import React, { forwardRef, useCallback, useMemo } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const BottomSheet = forwardRef<BottomSheetModal>((props, ref) => {
 	const snapPoints = useMemo(() => ['50%'], [])
+
+	const renderBackdrop = useCallback(
+		(props: any) => (
+			<BottomSheetBackdrop
+				appearsOnIndex={0}
+				disappearsOnIndex={-1}
+				{...props}
+			/>
+		),
+		[]
+	)
 
 	return (
 		<BottomSheetModal
 			snapPoints={snapPoints}
 			ref={ref}
 			overDragResistanceFactor={0}
+			backdropComponent={renderBackdrop}
 		>
 			<View>
 				<Text>BottomSheet</Text>
