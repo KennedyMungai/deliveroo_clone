00d44a7db81157a8347822b893349ce18b251d81
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 10:05:41 +0300

    Created and rendered out a custom header component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import CustomHeader from '@/components/CustomHeader'
 import { Stack } from 'expo-router'
 import React from 'react'
 
 const MainAppLayout = () => {
 	return (
 		<Stack>
-			<Stack.Screen name='index' />
+			<Stack.Screen
+				name='index'
+				options={{ header: () => <CustomHeader /> }}
+			/>
 		</Stack>
 	)
 }
