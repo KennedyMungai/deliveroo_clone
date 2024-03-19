7d12887b0b8063e8fd05ee2e8b030fa964c8914f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 10:09:21 +0300

    Used a SafeAreaView instead of a View and added some height and paddinh

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const CustomHeader = () => {
 	return (
-		<View>
+		<SafeAreaView style={styles.container}>
 			<Text>CustomHeader</Text>
-		</View>
+		</SafeAreaView>
 	)
 }
 
 export default CustomHeader
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		height: 150,
+		paddingTop: 50
+	}
+})
