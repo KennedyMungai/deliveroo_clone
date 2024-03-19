1c34789e6c8a3d1e00fd3eaa23e1f274e72f8042
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 14:46:49 +0300

    Added some footer shadow radius

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -35,7 +35,12 @@ const styles = StyleSheet.create({
 		bottom: 0,
 		left: 0,
 		right: 0,
-		height: 100
+		height: 100,
+		flex: 1,
+		elevation: 10,
+		shadowColor: 'black',
+		shadowOpacity: 0.1,
+		shadowRadius: 10
 	},
 	fullButton: {
 		backgroundColor: Colors.primary,
