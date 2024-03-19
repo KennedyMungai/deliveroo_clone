508c3e05d14365366d2bcb8676ca93f5a363c356
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 15:57:51 +0300

    Added some pressable items for the filter modal

--- app/(modal)/filter.tsx
+++ app/(modal)/filter.tsx
@@ -21,17 +21,66 @@ interface SomeCategory {
 }
 
 const ItemBox = () => (
-	<View style={styles.itemContainer}>
-		<TouchableOpacity style={styles.item}>
-			<Ionicons
-				name='arrow-down-outline'
-				size={20}
-				color={Colors.medium}
-			/>
-			<Text style={{ flex: 1 }}>Sort</Text>
-			<Ionicons name='chevron-forward' size={22} color={Colors.primary} />
-		</TouchableOpacity>
-	</View>
+	<>
+		<View style={styles.itemContainer}>
+			<TouchableOpacity style={styles.item}>
+				<Ionicons
+					name='arrow-down-outline'
+					size={20}
+					color={Colors.medium}
+				/>
+				<Text style={{ flex: 1 }}>Sort</Text>
+				<Ionicons
+					name='chevron-forward'
+					size={22}
+					color={Colors.primary}
+				/>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.item}>
+				<Ionicons
+					name='fast-food-outline'
+					size={20}
+					color={Colors.medium}
+				/>
+				<Text style={{ flex: 1 }}>Hygiene Rating</Text>
+				<Ionicons
+					name='chevron-forward'
+					size={22}
+					color={Colors.primary}
+				/>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.item}>
+				<Ionicons
+					name='pricetag-outline'
+					size={20}
+					color={Colors.medium}
+				/>
+				<Text style={{ flex: 1 }}>Offers</Text>
+				<Ionicons
+					name='chevron-forward'
+					size={22}
+					color={Colors.primary}
+				/>
+			</TouchableOpacity>
+
+			<TouchableOpacity style={styles.item}>
+				<Ionicons
+					name='nutrition-outline'
+					size={20}
+					color={Colors.medium}
+				/>
+				<Text style={{ flex: 1 }}>Dietary</Text>
+				<Ionicons
+					name='chevron-forward'
+					size={22}
+					color={Colors.primary}
+				/>
+			</TouchableOpacity>
+		</View>
+		<Text style={styles.header}>Categories</Text>
+	</>
 )
 
 const FilterModal = () => {
@@ -102,6 +151,11 @@ const styles = StyleSheet.create({
 	itemContainer: {
 		padding: 8,
 		borderRadius: 8,
+		marginBottom: 16
+	},
+	header: {
+		fontSize: 16,
+		fontWeight: 'bold',
 		marginBottom: 16
 	},
 	item: {
