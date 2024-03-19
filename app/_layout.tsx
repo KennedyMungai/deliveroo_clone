a8537a99ff48b8c86514dc2e17393ddaa19b7813
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 11:32:26 +0300

    Added a bottom sheet to the root layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import CustomHeader from '@/components/CustomHeader'
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { Stack } from 'expo-router'
 import React from 'react'
 
 const MainAppLayout = () => {
 	return (
-		<Stack>
-			<Stack.Screen
-				name='index'
-				options={{ header: () => <CustomHeader /> }}
-			/>
-		</Stack>
+		<BottomSheetModalProvider>
+			<Stack>
+				<Stack.Screen
+					name='index'
+					options={{ header: () => <CustomHeader /> }}
+				/>
+			</Stack>
+		</BottomSheetModalProvider>
 	)
 }
 
