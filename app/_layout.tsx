d6ce88bedfcada774c5afa9397e56c794335cd93
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-19 14:32:07 +0300

    Added an x icon to close the modal page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import CustomHeader from '@/components/CustomHeader'
+import { Ionicons } from '@expo/vector-icons'
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
-import { Stack } from 'expo-router'
+import { Stack, useNavigation } from 'expo-router'
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const MainAppLayout = () => {
+	const navigation = useNavigation()
+
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
 			<BottomSheetModalProvider>
@@ -20,7 +24,18 @@ const MainAppLayout = () => {
 							animation: 'slide_from_bottom',
 							headerTitle: 'Label',
 							headerShadowVisible: false,
-							headerTransparent: true
+							headerTransparent: true,
+							headerLeft: () => (
+								<TouchableOpacity
+									onPress={() => navigation.goBack()}
+								>
+									<Ionicons
+										name='close-outline'
+										size={24}
+										style={{ marginRight: 20 }}
+									/>
+								</TouchableOpacity>
+							)
 						}}
 					/>
 				</Stack>
