6225d2fe7bcbfbf9d50b28b56b5c6746ebdfaf3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 12:47:01 +0300

    Added some props to the MapView component

--- app/(modal)/locationSearch.tsx
+++ app/(modal)/locationSearch.tsx
@@ -4,15 +4,15 @@ import MapView from 'react-native-maps'
 
 const LocationSearch = () => {
 	const [location, setLocation] = useState({
-		latitude: 1,
-		longitude: 38,
+		latitude: 1.0,
+		longitude: 38.23,
 		latitudeDelta: 0.02,
 		longitudeDelta: 0.2
 	})
 
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} />
+			<MapView style={styles.map} region={location} />
 		</View>
 	)
 }
