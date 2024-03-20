a144e38514c09d4cf8bf03c0feae097c0d55034c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 14:56:56 +0300

    Rendered the categories inside a horizontal scroll view

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,14 +1,26 @@
+import { categories } from '@/assets/data/home'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 const Categories = () => {
 	return (
-		<View>
-			<Text>Categories</Text>
-		</View>
+		<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+			{categories.map((category, index) => (
+				<View style={styles.categoryCard} key={index}>
+					<Image source={category.img} />
+					<Text>{category.text}</Text>
+				</View>
+			))}
+		</ScrollView>
 	)
 }
 
 export default Categories
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	categoryCard: {
+		width: 100,
+		height: 100,
+		marginHorizontal: 10
+	}
+})
