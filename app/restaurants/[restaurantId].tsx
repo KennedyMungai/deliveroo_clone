79ef6ace6f9dd1118d33f684fdf1059d2ed20d41
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 15:14:09 +0300

    Created the restaurant details page

---
+++ app/restaurants/[restaurantId].tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RestaurantDetailsPage = () => {
+	return (
+		<View>
+			<Text>RestaurantDetailsPage</Text>
+		</View>
+	)
+}
+
+export default RestaurantDetailsPage
+
+const styles = StyleSheet.create({})
