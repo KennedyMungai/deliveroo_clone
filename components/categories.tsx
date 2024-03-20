0f880d2f2a84d981bc27d18a8603913c5c6ad63e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 15:03:01 +0300

    Adde reduced the shadow intensity for the cards

--- components/categories.tsx
+++ components/categories.tsx
@@ -7,8 +7,8 @@ const Categories = () => {
 		<ScrollView horizontal showsHorizontalScrollIndicator={false}>
 			{categories.map((category, index) => (
 				<View style={styles.categoryCard} key={index}>
-					<Image source={category.img} />
-					<Text>{category.text}</Text>
+					<Image source={category.img} style={{ borderRadius: 4 }} />
+					<Text style={styles.categoryText}>{category.text}</Text>
 				</View>
 			))}
 		</ScrollView>
@@ -21,6 +21,18 @@ const styles = StyleSheet.create({
 	categoryCard: {
 		width: 100,
 		height: 100,
-		marginHorizontal: 10
+		marginHorizontal: 10,
+		elevation: 2,
+		shadowColor: '#00000050',
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		margin: 2
+	},
+	categoryText: {
+		padding: 5,
+		fontSize: 12,
+		fontWeight: 'bold'
 	}
 })
