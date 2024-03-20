3c7c9aaf78eeff4c57b99fa2aeb6052a388c3304
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 15:23:47 +0300

    Rendered out the cards with the hotel info

--- components/restaurants.tsx
+++ components/restaurants.tsx
@@ -1,15 +1,27 @@
 import { restaurants } from '@/assets/data/home'
+import { Link } from 'expo-router'
 import React from 'react'
-import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import {
+	Image,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TouchableOpacity
+} from 'react-native'
 
 const Restaurants = () => {
 	return (
 		<ScrollView horizontal showsHorizontalScrollIndicator={false}>
 			{restaurants.map((hotel, index) => (
-				<View style={styles.categoryCard} key={index}>
-					<Image source={hotel.img} style={{ borderRadius: 4 }} />
-					<Text style={styles.categoryText}>{hotel.name}</Text>
-				</View>
+				<Link href={`restaurants/${hotel.id}`} key={index} asChild>
+					<TouchableOpacity style={styles.categoryCard} key={index}>
+						<Image
+							source={hotel.img}
+							style={{ borderRadius: 4, aspectRatio: 1 }}
+						/>
+						<Text style={styles.categoryText}>{hotel.name}</Text>
+					</TouchableOpacity>
+				</Link>
 			))}
 		</ScrollView>
 	)
@@ -19,15 +31,17 @@ export default Restaurants
 
 const styles = StyleSheet.create({
 	categoryCard: {
-		width: 100,
-		height: 100,
-		marginHorizontal: 10,
+		width: 300,
+		height: 250,
+		marginHorizontal: 100,
 		elevation: 2,
 		shadowColor: '#00000050',
 		shadowOffset: {
 			width: 0,
 			height: 4
-		}
+		},
+		display: 'flex',
+		gap: 20
 	},
 	categoryText: {
 		padding: 5,
