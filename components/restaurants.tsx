cf9b0449bf232795e80c0c99946a78f90c1a2136
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-20 15:11:25 +0300

    Rendered the restaurant data

--- components/restaurants.tsx
+++ components/restaurants.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { restaurants } from '@/assets/data/home'
 import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 
 const Restaurants = () => {
 	return (
-		<View>
-			<Text>Restaurants</Text>
-		</View>
+		<ScrollView horizontal showsHorizontalScrollIndicator={false}>
+			{restaurants.map((hotel, index) => (
+				<View style={styles.categoryCard} key={index}>
+					<Image source={hotel.img} style={{ borderRadius: 4 }} />
+					<Text style={styles.categoryText}>{hotel.name}</Text>
+				</View>
+			))}
+		</ScrollView>
 	)
 }
 
 export default Restaurants
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	categoryCard: {
+		width: 100,
+		height: 100,
+		marginHorizontal: 10,
+		elevation: 2,
+		shadowColor: '#00000050',
+		shadowOffset: {
+			width: 0,
+			height: 4
+		}
+	},
+	categoryText: {
+		padding: 5,
+		fontSize: 12,
+		fontWeight: 'bold',
+		textAlign: 'center'
+	}
+})
