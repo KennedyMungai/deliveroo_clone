8a6bb88cf27a2f9eb0cdd321bea4e8eba75cd183
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 07:47:43 +0300

    Made some changes to the component

--- app/restaurants/[restaurantId].tsx
+++ app/restaurants/[restaurantId].tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import ParallaxScrollView from '@/components/parallax-scroll-view'
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
 const RestaurantDetailsPage = () => {
 	return (
