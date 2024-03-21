d9b239048d8338f21cdfea91e790ed49af8252b8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 16:01:04 +0300

    Rendering out the items inside the basket

--- app/basket.tsx
+++ app/basket.tsx
@@ -1,16 +1,28 @@
 import useBasketStore from '@/store/basket-store'
-import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 const BasketPage = () => {
+	const [order, setOrder] = useState(false)
+
 	const { addProduct, clearCart, items, products, reduceProduct, total } =
 		useBasketStore()
 
 	return (
 		<SafeAreaView style={styles.container}>
-			<View>
-				<Text>BasketPage</Text>
-			</View>
+			{order && <Text>Cool Order</Text>}
+			{!order && (
+				<>
+					<FlatList
+						data={products}
+						renderItem={({ item }) => (
+							<View>
+								<Text>{item.name}</Text>
+							</View>
+						)}
+					/>
+				</>
+			)}
 		</SafeAreaView>
 	)
 }
@@ -19,6 +31,7 @@ export default BasketPage
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		backgroundColor: 'white',
 		paddingHorizontal: 10,
 		paddingTop: 100
