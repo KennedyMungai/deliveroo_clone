e6ba27b1ae7984ed57b51a78956b45424f81b7c9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 12:13:30 +0300

    Created the file for the dish modal

---
+++ app/(modal)/dish.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const DishDetailPage = () => {
+	return (
+		<View>
+			<Text>DishDetailPage</Text>
+		</View>
+	)
+}
+
+export default DishDetailPage
+
+const styles = StyleSheet.create({})
