9faad7d9db3cc5e7c228558da3f4401506a89bd7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 09:04:04 +0300

    Added some opacity to the background of the back button

--- app/restaurants/[restaurantId].tsx
+++ app/restaurants/[restaurantId].tsx
@@ -1,10 +1,34 @@
 import { restaurant } from '@/assets/data/restaurant'
 import ParallaxScrollView from '@/components/parallax-scroll-view'
 import Colors from '@/constants/Colors'
-import React from 'react'
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import { useNavigation } from 'expo-router'
+import React, { useLayoutEffect } from 'react'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 const RestaurantDetailsPage = () => {
+	const navigation = useNavigation()
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerTransparent: true,
+			headerTitle: '',
+			headerTintColor: Colors.primary,
+			headerLeft: () => (
+				<TouchableOpacity
+					onPress={() => navigation.goBack()}
+					style={styles.roundButton}
+				>
+					<Ionicons
+						name='arrow-back'
+						size={24}
+						color={Colors.primary}
+					/>
+				</TouchableOpacity>
+			)
+		})
+	}, [])
+
 	return (
 		<>
 			<ParallaxScrollView
@@ -12,7 +36,14 @@ const RestaurantDetailsPage = () => {
 				backgroundColor='white'
 				contentBackgroundColor={Colors.lightGrey}
 				parallaxHeaderHeight={300}
+				stickyHeaderHeight={50}
 				renderBackground={() => <Image source={restaurant.img} />}
+				renderStickyHeader={() => (
+					<View
+						key='sticky-header'
+						style={styles.stickySection}
+					></View>
+				)}
 			>
 				<View style={styles.detailsContainer}>
 					<Text>Details</Text>
@@ -32,5 +63,16 @@ const styles = StyleSheet.create({
 	},
 	detailsContainer: {
 		backgroundColor: Colors.lightGrey
+	},
+	stickySection: {
+		backgroundColor: 'red'
+	},
+	roundButton: {
+		width: 40,
+		height: 40,
+		borderRadius: 20,
+		backgroundColor: '#FFFFFF90',
+		alignItems: 'center',
+		justifyContent: 'center'
 	}
 })
