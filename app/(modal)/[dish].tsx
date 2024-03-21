08938fbc73a636dc1cc6d9c107553faae81dba9e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 12:49:59 +0300

    Added some styles for the modal page

--- app/(modal)/[dish].tsx
+++ app/(modal)/[dish].tsx
@@ -1,17 +1,29 @@
+import { getDishById } from '@/assets/data/restaurant'
 import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 const DishDetailPage = () => {
 	const { dish: dishId } = useLocalSearchParams()
 
+	const item = getDishById(+dishId)
+
 	return (
-		<View>
-			<Text>{dishId}</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View>
+				<Text>{dishId}</Text>
+			</View>
+		</SafeAreaView>
 	)
 }
 
 export default DishDetailPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		paddingHorizontal: 10,
+		paddingTop: 100
+	}
+})
