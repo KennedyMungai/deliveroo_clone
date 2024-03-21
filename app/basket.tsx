1f34fd213dec15930eaca41f83912bb0f38d53a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 15:55:59 +0300

    Grabbed all the items from the BasketSTore

--- app/basket.tsx
+++ app/basket.tsx
@@ -1,7 +1,11 @@
+import useBasketStore from '@/store/basket-store'
 import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 const BasketPage = () => {
+	const { addProduct, clearCart, items, products, reduceProduct, total } =
+		useBasketStore()
+
 	return (
 		<SafeAreaView style={styles.container}>
 			<View>
