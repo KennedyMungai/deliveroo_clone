15edfaa5ca7464b68ed69be5207abad4ae6d40e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 15:50:41 +0300

    Defined the basket page

---
+++ app/basket.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BasketPage = () => {
+	return (
+		<View>
+			<Text>BasketPage</Text>
+		</View>
+	)
+}
+
+export default BasketPage
+
+const styles = StyleSheet.create({})
