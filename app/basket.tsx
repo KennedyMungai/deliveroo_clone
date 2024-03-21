076dd4d5585cc019e5dd5aca37272e2e9c8d81cb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 15:55:07 +0300

    Styling the basket pahe

--- app/basket.tsx
+++ app/basket.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 
 const BasketPage = () => {
 	return (
-		<View>
-			<Text>BasketPage</Text>
-		</View>
+		<SafeAreaView style={styles.container}>
+			<View>
+				<Text>BasketPage</Text>
+			</View>
+		</SafeAreaView>
 	)
 }
 
 export default BasketPage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'white',
+		paddingHorizontal: 10,
+		paddingTop: 100
+	}
+})
