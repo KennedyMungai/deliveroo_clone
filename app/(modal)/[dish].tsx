a24fa2be416fefa097882c82c9d49b802c0ac479
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-21 12:42:57 +0300

    Got the dishId using localSearchParams

--- app/(modal)/[dish].tsx
+++ app/(modal)/[dish].tsx
@@ -1,10 +1,13 @@
+import { useLocalSearchParams } from 'expo-router'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const DishDetailPage = () => {
+	const { dish: dishId } = useLocalSearchParams()
+
 	return (
 		<View>
-			<Text>DishDetailPage</Text>
+			<Text>{dishId}</Text>
 		</View>
 	)
 }
